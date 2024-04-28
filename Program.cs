74427ee796e314bb5275f075348f274d49927501
Radu Todiras <beradion@gmail.com>
2024-04-28 12:50:17 +0300

    added print function and called both functions

--- Program.cs
+++ Program.cs
@@ -9,32 +9,55 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
-string[] FilterArray (string[] array)
+string[] FilterArray(string[] array)
 {
     int count = 0;
-    foreach (string str in array) 
+    foreach (string str in array)
     {
         if (str.Length <= 3)
         {
-            count++;    
+            count++;
         }
-        
     }
     string[] filtered = new string[count];
-        foreach (string str in array) 
+    int i = 0;
+    foreach (string str in array)
     {
-        int i = 0;
         if (str.Length <= 3)
         {
             filtered[i] = str;
-            i++;    
+            i++;
         }
-        
     }
     return filtered;
 }
+void PrintArray(string[] array)
+{
+    if (array.Length == 0)
+    {
+        Console.WriteLine("");
+        return;
+    }
+    else
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write($"{array[i]} ");
+        }
+
+    }
+}
 
 string[] array = { "Hello", "2", "world", ":-)" };
-string[] array2 = {"1234", "1567", "-2", "computer science" };
+string[] array2 = { "1234", "1567", "-2", "computer science" };
 string[] array3 = { "Russia", "Denmark", "Kazan" };
 
+string[] filtered = FilterArray(array);
+string[] filtered2 = FilterArray(array2);
+string[] filtered3 = FilterArray(array3);
+
+PrintArray(filtered);
+Console.WriteLine("");
+PrintArray(filtered2);
+Console.WriteLine("");
+PrintArray(filtered3);
